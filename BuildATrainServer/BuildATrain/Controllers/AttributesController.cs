6fb3e9206092603befa4d5ca4643eec949bea75d
Lisa Nolte <lisan@bbd.co.za>
2023-07-20 20:46:55 +0200

    Adding repository design pattern for entity framework db, also added example with attributes.

---
+++ BuildATrainServer/BuildATrain/Controllers/AttributesController.cs
@@ -0,0 +1,22 @@
+﻿using BuildATrain.Database.Models;
+using BuildATrain.Database.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildATrain.Controllers
+{
+    public class AttributesController : Controller
+    {
+        private readonly IRepository<Attributes> _attributesRepository;
+
+        public AttributesController(IRepository<Attributes> attributesRepository)
+        {
+            _attributesRepository = attributesRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var entities = await _attributesRepository.GetAllAsync();
+            return View(entities);
+        }
+    }
+}
