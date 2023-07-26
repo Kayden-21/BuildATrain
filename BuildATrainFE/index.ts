27d2e494c7deb046c95faf9bf0dca5fd2498ba4c
Kayden Kara <kayden@bbd.co.za>
2023-07-26 21:14:55 +0200

    Added redirect middleware and kept email in session to be accessed

--- BuildATrainFE/index.ts
+++ BuildATrainFE/index.ts
@@ -1,11 +1,20 @@
 import express from 'express';
 import passport from 'passport';
-import session from 'express-session';
+import session, { SessionData } from 'express-session';
 import GoogleStrategy from 'passport-google-oauth20';
 import { Profile } from 'passport-google-oauth20';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+// Declare merged types here
+declare module 'express-session' {
+  interface SessionData {
+    passport?: {
+      user?: any;
+    };
+  }
+}
 
 const app = express();
 const port = 4000;
@@ -18,6 +27,13 @@ app.use(session({
   saveUninitialized: false,
 }));
 
+function ensureAuthenticated(req: express.Request, res: express.Response, next: express.NextFunction) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
 // Register Google Strategy
 passport.use(
   new GoogleStrategy.Strategy(
@@ -27,22 +43,27 @@ passport.use(
       callbackURL: 'http://localhost:4000/auth/google/callback',
     },
     (accessToken: string, refreshToken: any, profile: Profile, done: (error: any, user?: any, info?: any) => void) => {
-      // Here, you should save user data to a database and call done()
-      done(null, profile);
+      const user = {
+        id: profile.id,
+        email: profile.emails ? profile.emails[0].value : null
+      };
+      done(null, user);
     }
   )
 );
+
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.serializeUser((user: any, done) => {
-  done(null, user.id);
+  done(null, user);
 });
 
-passport.deserializeUser((id, done) => {
-  done(null, { id: id });
+passport.deserializeUser((user: any, done) => {
+  done(null, user);
 });
+
 
 // Google authentication route
 app.get(
@@ -66,6 +87,15 @@ app.get(
 app.get('/login', (req, res) => {
   res.sendFile(__dirname + '/src/login.html');
 });
+
+app.get('/some-route', ensureAuthenticated, (req: express.Request, res: express.Response) => {
+  // This code will only be executed if the user is authenticated.
+  // Here we are asserting that req.user will exist
+  if(req.session && req.session.passport && req.session.passport.user){
+    console.log(req.session.passport.user.email);
+  } 
+});
+
 
 
 app.listen(4000, () => {
